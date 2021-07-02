b9c7fe3398698620f29c862781c0f351765e5489
Facundo Zallocco <facundozallocco@gmail.com>
2021-07-01 22:35:33 -0300

    Anda el url

--- TP_Final_SIM/TP_Final_SIM/urls.py
+++ TP_Final_SIM/TP_Final_SIM/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from TP_Final_SIM.ejercicio14 import views
+from ejercicio14 import views
 
 urlpatterns = [
     path('', views.index),
+    path('acercade/', views.acercade),
 ]
